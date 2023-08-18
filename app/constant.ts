24f07105e5bb9bdc2b9712b3fd4f0003fbc2c9c1
徐洋 <xuyang1>
2023-08-18 20:35:26 +0800

    优化代理地址

--- app/constant.ts
+++ app/constant.ts
@@ -9,7 +9,7 @@ export const FETCH_TAG_URL = `https://api.github.com/repos/${OWNER}/${REPO}/tags
 export const RUNTIME_CONFIG_DOM = "danger-runtime-config";
 // export const DEFAULT_API_HOST = "https://chatgpt1.nextweb.fun/api/proxy";
 // export const DEFAULT_API_HOST = "http://localhost:9906/api/openai";
-export const DEFAULT_API_HOST = "http://api.lchainl.com/api/openai";
+export const DEFAULT_API_HOST = "https://api.lchainl.com/api/openai";
 
 export enum Path {
   Home = "/",
